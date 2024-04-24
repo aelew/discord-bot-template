e7dc876b96a3f7b3785c60ff71627afaff52800f
aelew <118043566+aelew@users.noreply.github.com>
2024-04-23 19:49:24 -0700

    improve prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 /** @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig } */
 const config = {
   plugins: ['@ianvs/prettier-plugin-sort-imports'],
-  importOrder: ['<THIRD_PARTY_MODULES>', '', '^@/', '^[../]', '^[./]'],
-  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrder: ['<THIRD_PARTY_MODULES>', '', '^[../]', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
   trailingComma: 'none',
   singleQuote: true
 };
